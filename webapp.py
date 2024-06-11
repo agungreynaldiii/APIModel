4ed1d2fb38482bd02f225b8d4a55b1a9474809c3
Agung Reynaldi <agungreynaldiii@gmail.com>
2024-06-11 14:55:10 +0700

    Update Additional Information

--- webapp.py
+++ webapp.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, jsonify, send_from_directory
 from flask_cors import CORS
 import os
 from ultralytics import YOLO
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -14,16 +15,26 @@ DETECT_FOLDER = 'runs/detect'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(DETECT_FOLDER, exist_ok=True)
 
+# Read CSV file once when the server starts
+food_info = pd.read_csv('food.csv')
+
+def get_food_info(label):
+    info = food_info[food_info['Makanan'].str.lower() == label.lower()]
+    if not info.empty:
+        return info.to_dict(orient='records')[0]
+    else:
+        return None
+
 @app.route("/", methods=["GET", "POST"])
 def predict_img():
     if request.method == "POST":
         if 'file' in request.files:
             f = request.files['file']
             basepath = os.path.dirname(__file__)
-            filepath = os.path.join(basepath, UPLOAD_FOLDER, f.filename) # type: ignore
+            filepath = os.path.join(basepath, UPLOAD_FOLDER, f.filename)
             f.save(filepath)
             
-            file_extension = f.filename.rsplit('.', 1)[1].lower() # type: ignore
+            file_extension = f.filename.rsplit('.', 1)[1].lower()
             
             model = YOLO('best.pt')
 
@@ -32,7 +43,14 @@ def predict_img():
                 detections = model(img, save=True)
 
                 # Extract the class names from the detections
-                labels = [model.names[int(cls)] for cls in detections[0].boxes.cls] # type: ignore
+                labels = [model.names[int(cls)] for cls in detections[0].boxes.cls]
+
+                # Get additional info from CSV
+                additional_info = []
+                for label in labels:
+                    info = get_food_info(label)
+                    if info:
+                        additional_info.append(info)
 
                 # Save the detected image with a generic name
                 detected_img_path = os.path.join(DETECT_FOLDER, 'image0.jpg')
@@ -41,7 +59,7 @@ def predict_img():
                 # Full URL to the detected image
                 full_detected_img_path = f"http://{request.host}/detect/image0.jpg"
 
-                return jsonify({'labels': labels, 'filename': f.filename, 'detected_img_path': full_detected_img_path})
+                return jsonify({'labels': labels, 'filename': f.filename, 'detected_img_path': full_detected_img_path, 'additional_info': additional_info})
             
     return render_template('index.html')
 
